d167a26b7f2375da261fa58de8d10c8c4d3f212f
Benjamin Besse <benjamin.besse02@gmail.com>
2016-09-27 14:41:03 +0200

    @ modification de l'initialisation des sockets + ajout du unbind des sockets

--- js/libs/backbone/views/base.js
+++ js/libs/backbone/views/base.js
@@ -240,6 +240,7 @@ module.exports = View.extend({
         this.unmountAllTags();
         this._unbindStikits();
         this._removeGlobalEvents();
+        this._unbindSockets();
         $(this.el).remove();
         this.remove();
         Backbone.View.prototype.remove.call(this);
@@ -439,29 +440,43 @@ module.exports = View.extend({
             return;
         }
 
-        this._sockets = {};
-
         if (this.socketEvents && !_.isEmpty(this.socket)) {
-            this._socket = io(this.socket);
+            this.socket = io(this.socket);
 
             _.each(this.socketEvents.default, function (funcName, event) {
                 if (_.isFunction(this[funcName])) {
                     _.bindAll(this, funcName);
-                    this._socket.on(event, this[funcName]);
+                    this.socket.on(event, this[funcName]);
                 }
             }, this);
         }
 
         _.each(this.sockets, function (url, socketName) {
-            this._sockets[socketName] = io(url);
+            this.sockets[socketName] = io(url);
 
             _.each(this.socketEvents[socketName], function (funcName, event) {
                 if (_.isFunction(this[funcName])) {
                     _.bindAll(this, funcName);
-                    this._sockets[socketName].on(event, this[funcName]);
+                    this.sockets[socketName].on(event, this[funcName]);
                 }
             }, this);
         }, this);
+    },
+    /**
+     * Supprime tous les événements sockets de la vue
+     *
+     * @private
+     */
+    _unbindSockets : function unbindSocket() {
+        if (this.socket) {
+            this.socket.removeAllListeners();
+            this.socket.disconnect();
+        }
+
+        _.each(this.sockets, function (socket) {
+            socket.removeAllListeners();
+            socket.disconnect();
+        });
     },
     // fonctions de base vides
     render              : function render() {},
