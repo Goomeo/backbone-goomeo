44ce61e778438978b3c5923b3181b6801669ad8f
Benjamin Besse <benjamin.besse02@gmail.com>
2016-09-28 10:42:28 +0200

    @ test eventManager

--- js/libs/backbone/views/base.js
+++ js/libs/backbone/views/base.js
@@ -40,7 +40,6 @@ module.exports = View.extend({
         this.name               = options.name;
         this.tags               = {};
 
-        this._initGlobal();
         this._initGlobalEvents();
         this._initSockets();
 
@@ -118,6 +117,29 @@ module.exports = View.extend({
                 eventManager.listenToOnce.apply(this, arguments);
             }.bind(this)
         };
+    },
+    global : {
+        on : function on(event, func, ctx) {
+            eventManager.on(event, func, ctx);
+        }.bind(this),
+        off : function off() {
+            eventManager.off.apply(this, arguments);
+        }.bind(this),
+        trigger : function trigger() {
+            eventManager.trigger.apply(this, arguments);
+        }.bind(this),
+        once : function once() {
+            eventManager.once.apply(this, arguments);
+        }.bind(this),
+        listenTo : function listenTo() {
+            eventManager.listenTo.apply(this, arguments);
+        }.bind(this),
+        stopListening : function stopListening() {
+            eventManager.stopListening.apply(this, arguments);
+        }.bind(this),
+        listenToOnce : function listenToOnce() {
+            eventManager.listenToOnce.apply(this, arguments);
+        }.bind(this)
     },
     panels : {
         open : function open() {
