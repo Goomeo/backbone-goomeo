0bdf67a1602b434c2ec3a901fb361a87ea5b418a
Benjamin Besse <benjamin.besse02@gmail.com>
2016-10-07 09:13:59 +0200

    @ correction du showNetworkModal

--- js/libs/goomeo/panelManager.js
+++ js/libs/goomeo/panelManager.js
@@ -186,7 +186,7 @@ module.exports = _.extend({
     },
     _showNetworkModal : function showNetworkModal() {
         modalManager.open({
-            view : this.createSubView('modal:network', require('../../modules/common/modals/network'))
+            view : viewManager.create('modal:network', require('../../modules/common/modals/network'))
         });
     }
 }, Backbone.Events);
