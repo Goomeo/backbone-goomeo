4906d6941a4a3860bcd9f19be0379afd774612ed
Benjamin Besse <benjamin.besse02@gmail.com>
2016-11-03 11:23:12 +0100

    @ changement des events sur les models/collections pour la partie socket

--- js/libs/backbone/collections/base.js
+++ js/libs/backbone/collections/base.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const _         = require('underscore');
-const Backbone  = require('backbone');
-const io        = require('socket.io-client');
-const functions = require('../functions');
+const _             = require('underscore');
+const Backbone      = require('backbone');
+const socketManager = require('../../goomeo/socketManager');
+const functions     = require('../functions');
 
 var Collection = Backbone.Collection.extend(functions);
 
@@ -24,41 +24,51 @@ module.exports = Collection.extend({
         }
 
         if (this.socketEvents && !_.isEmpty(this.socket)) {
-            this.socket = io(this.socket);
+            this._socketUrl = this.socket;
+            this.socket     = socketManager.create(this.socket);
 
-            _.each(this.socketEvents.default, function (funcName, event) {
+            _.each(this.socketEvents.default, (funcName, event) => {
                 if (_.isFunction(this[funcName])) {
                     _.bindAll(this, funcName);
                     this.socket.on(event, this[funcName]);
                 }
-            }, this);
+            });
         }
 
-        _.each(this.sockets, function (url, socketName) {
-            this.sockets[socketName] = io(url);
+        this._socketsUrl = this.sockets;
+        _.each(this.sockets, (url, socketName) => {
+            this.sockets[socketName] = socketManager.create(url);
 
-            _.each(this.socketEvents[socketName], function (funcName, event) {
+            _.each(this.socketEvents[socketName], (funcName, event) => {
                 if (_.isFunction(this[funcName])) {
                     _.bindAll(this, funcName);
                     this.sockets[socketName].on(event, this[funcName]);
                 }
-            }, this);
-        }, this);
+            });
+        });
     },
     /**
-     * Supprime tous les événements sockets de la collection
+     * Supprime tous les événements sockets du model
      *
      * @private
      */
     _unbindSockets : function unbindSocket() {
         if (this.socket) {
-            this.socket.removeAllListeners();
-            this.socket.disconnect();
+            socketManager.unbind({
+                url         : this._socketUrl,
+                listeners   : _.mapObject(this.socketEvents.default, (funcName) => {
+                    return this[funcName];
+                })
+            });
         }
 
-        _.each(this.sockets, function (socket) {
-            socket.removeAllListeners();
-            socket.disconnect();
+        _.each(this.sockets, (socket, name) => {
+            socketManager.unbind({
+                url         : this._socketsUrl[name],
+                listeners   : _.mapObject(this.socketEvents[name], (funcName) => {
+                    return this[funcName];
+                })
+            });
         });
     }
 });
--- js/libs/backbone/models/base.js
+++ js/libs/backbone/models/base.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const _         = require('underscore');
-const Backbone  = require('backbone');
-const io        = require('socket.io-client');
-const functions = require('../functions');
+const _             = require('underscore');
+const Backbone      = require('backbone');
+const socketManager = require('../../goomeo/socketManager');
+const functions     = require('../functions');
 
 var Model       = Backbone.Model.extend(functions);
 
@@ -24,26 +24,28 @@ module.exports  = Model.extend({
         }
 
         if (this.socketEvents && !_.isEmpty(this.socket)) {
-            this.socket = io(this.socket);
+            this._socketUrl = this.socket;
+            this.socket     = socketManager.create(this.socket);
 
-            _.each(this.socketEvents.default, function (funcName, event) {
+            _.each(this.socketEvents.default, (funcName, event) => {
                 if (_.isFunction(this[funcName])) {
                     _.bindAll(this, funcName);
                     this.socket.on(event, this[funcName]);
                 }
-            }, this);
+            });
         }
 
-        _.each(this.sockets, function (url, socketName) {
-            this.sockets[socketName] = io(url);
+        this._socketsUrl = this.sockets;
+        _.each(this.sockets, (url, socketName) => {
+            this.sockets[socketName] = socketManager.create(url);
 
-            _.each(this.socketEvents[socketName], function (funcName, event) {
+            _.each(this.socketEvents[socketName], (funcName, event) => {
                 if (_.isFunction(this[funcName])) {
                     _.bindAll(this, funcName);
                     this.sockets[socketName].on(event, this[funcName]);
                 }
-            }, this);
-        }, this);
+            });
+        });
     },
     /**
      * Supprime tous les événements sockets du model
@@ -52,13 +54,21 @@ module.exports  = Model.extend({
      */
     _unbindSockets : function unbindSocket() {
         if (this.socket) {
-            this.socket.removeAllListeners();
-            this.socket.disconnect();
+            socketManager.unbind({
+                url         : this._socketUrl,
+                listeners   : _.mapObject(this.socketEvents.default, (funcName) => {
+                    return this[funcName];
+                })
+            });
         }
 
-        _.each(this.sockets, function (socket) {
-            socket.removeAllListeners();
-            socket.disconnect();
+        _.each(this.sockets, (socket, name) => {
+            socketManager.unbind({
+                url         : this._socketsUrl[name],
+                listeners   : _.mapObject(this.socketEvents[name], (funcName) => {
+                    return this[funcName];
+                })
+            });
         });
     }
 });
